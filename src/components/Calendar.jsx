924a2593c7bced08a07693982b0c6318b765497f
Elonmusk <srujanaj313@gmail.com>
2024-06-18 23:35:57 +0530

    git_ignore file added

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,8 +3,18 @@ import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./index.css";
 
 function Calendar() {
+  const events = [
+    {
+      title: "Srujana Meeting",
+      start: "2024-06-19T08:00:00",
+      end: "2024-06-19T09:00:00",
+    },
+  ];
   return (
     <div>
       <Fullcalendar
@@ -16,6 +26,18 @@ function Calendar() {
           end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
         }}
         height={"90vh"}
+        events={events}
+        eventDidMount={(info) => {
+          return new bootstrap.Popover(info.el, {
+            title: info.event.title,
+            placement: "auto",
+            trigger: "hover",
+            customClass: "popoverStyle",
+            content:
+              "<p>Please subscribe<strong>Bootstrap popover</strong>.</p>",
+            html: true,
+          });
+        }}
       />
     </div>
   );
