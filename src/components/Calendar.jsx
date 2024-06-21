7a766bbca24a26b7696fad5f153dbfd569f431e8
Elonmusk <srujanaj313@gmail.com>
2024-06-21 09:01:00 +0530

    formik with yup

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,36 +1,43 @@
+// FullCalendarComponent.jsx
 import React, { useState } from "react";
-import Fullcalendar from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-// import * as bootstrap from "bootstrap";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import { Modal, Button, Form, Row, Col } from "react-bootstrap";
+import { Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "./index.css";
-
-function Calendar() {
+const FullCalendarComponent = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalMode, setModalMode] = useState("create");
+  // 'create' or 'view'
   const [meetingData, setMeetingData] = useState({
     title: "",
+    requiredEmails: "",
+    optionalEmails: "",
     startDate: "",
     startTime: "",
     endDate: "",
     endTime: "",
     location: "",
-    requiredEmails: "",
-    optionalEmails: "",
   });
-  const [titleErr,setTitleErr] = useState('');
-  const [events, setEvents] = useState([
-    {
-      title: "Srujana Meeting",
-      start: "2024-06-19T08:00:00",
-      end: "2024-06-19T09:00:00",
-      requiredEmails:"S@gmail.com",
-      optionalEmails:"SR@gmail.com"
-    },
-  ]);
+  const [events, setEvents] = useState([]);
+  const handleDateClick = (arg) => {
+    const initialDate = arg.dateStr;
+    setMeetingData({
+      title: "",
+      requiredEmails: "",
+      optionalEmails: "",
+      startDate: initialDate,
+      startTime: "08:00",
+      endDate: initialDate,
+      endTime: "09:00",
+      location: "",
+    });
+    setModalMode("create");
+    setShowModal(true);
+  };
   const handleEventClick = (info) => {
     const event = info.event;
     const startDateTime = event.start.toISOString().slice(0, 16);
@@ -45,225 +52,227 @@ function Calendar() {
       endTime: endDateTime.split("T")[1],
       location: event.extendedProps.location,
     });
-    setTitleErr('');
     setModalMode("view");
     setShowModal(true);
   };
-
-  const handleDateClick = (arg) => {
-    const initialDate = arg.dateStr;
-    setMeetingData({
-      title: "",
-      startDate: initialDate,
-      startTime: "08:00",
-      endDate: initialDate,
-      endTime: "09:00",
-      location: "",
-      requiredEmails: "",
-      optionalEmails: "",
-    });
-    setTitleErr('');
-    setModalMode("create");
-    setShowModal(true);
-  };
   const handleClose = () => setShowModal(false);
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setMeetingData((prevData) => ({ ...prevData, [name]: value }));
-  };
-  const handleSave = () => {
-    if(!meetingData.title.trim()){
-      setTitleErr('Meeting Title is Required')
-      return;
-    }
-    const { startDate, startTime, endDate, endTime } = meetingData;
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Meeting title is required."),
+    requiredEmails: Yup.string()
+      .required("Required emails are required.")
+      .matches(
+        /^(\s*[\w.-]+@affiliate\.nhes\.nh\.gov\s*;\s*)*$/,
+        "Emails must be valid and separated by semicolons."
+      ),
+    optionalEmails: Yup.string().matches(
+      /^(\s*[\w.-]+@affiliate\.nhes\.nh\.gov\s*;\s*)*$/,
+      "Emails must be valid and separated by semicolons."
+    ),
+    startDate: Yup.date().required("Start date is required."),
+    startTime: Yup.string().required("Start time is required."),
+    endDate: Yup.date().required("End date is required."),
+    endTime: Yup.string().required("End time is required."),
+  });
+  const handleSave = (values) => {
+    const {
+      title,
+      startDate,
+      startTime,
+      endDate,
+      endTime,
+      location,
+      requiredEmails,
+      optionalEmails,
+    } = values;
     const formattedStartDateTime = `${startDate}T${startTime}:00`;
     const formattedEndDateTime = `${endDate}T${endTime}:00`;
-    const savedData = {
-      ...meetingData,
-      startDateTime: formattedStartDateTime,
-      endDateTime: formattedEndDateTime,
+    const newEvent = {
+      title,
+      start: formattedStartDateTime,
+      end: formattedEndDateTime,
+      extendedProps: { location, requiredEmails, optionalEmails },
     };
-    // console.log("Meeting data:", savedData);
-    const eventData = {
-      title:savedData.title,
-      start: savedData.startDateTime,
-      end: savedData.endDateTime,
-      requiredEmails:savedData.requiredEmails,
-      optionalEmails:savedData.optionalEmails
-    };
-    setEvents((event) => [...event,eventData]);
+    setEvents([...events, newEvent]);
     setShowModal(false);
   };
-  
-  
   return (
     <div>
-      <Fullcalendar
+      <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView={"dayGridMonth"}
+        initialView="dayGridMonth"
+        dateClick={handleDateClick}
+        events={events}
+        eventClick={handleEventClick}
         headerToolbar={{
           start: "today prev,next", // will normally be on the left. if RTL, will be on the right
           center: "title",
           end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
         }}
-        height={"90vh"}
-        dateClick={handleDateClick}
-        events={events}
-        eventClick={handleEventClick}
-        // eventDidMount={(info) => {
-        //   return new bootstrap.Popover(info.el, {
-        //     title: info.event.title,
-        //     placement: "auto",
-        //     trigger: "hover",
-        //     customClass: "popoverStyle",
-        //     content:
-        //       "<p>Please subscribe<strong>Bootstrap popover</strong>.</p>",
-        //     html: true,
-        //   });
-        // }}
       />
       <Modal show={showModal} onHide={handleClose}>
-        
         <Modal.Header closeButton>
-          
           <Modal.Title>
             {modalMode === "create" ? "Schedule Meeting" : "View Meeting"}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          
-          <Form>
-            
-            <Form.Group controlId="meetingTitle">
-              
-              <Form.Label>Meeting Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter meeting title"
-                name="title"
-                value={meetingData.title}
-                onChange={handleInputChange}
-                readOnly={modalMode === "view"}
-                isInvalid={!!titleErr}
-              />
-              <Form.Control.Feedback type="invalid">
-                {titleErr}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="requiredEmails">
-              <Form.Label>Required</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Required Emails"
-                name="requiredEmails"
-                value={meetingData.requiredEmails}
-                onChange={handleInputChange}
-                readOnly={modalMode === "view"}
-              />
-            </Form.Group>
-            <Form.Group controlId="optionalEmails">
-              <Form.Label>Optional</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Optional Emails"
-                name="optionalEmails"
-                value={meetingData.optionalEmails}
-                onChange={handleInputChange}
-                readOnly={modalMode === "view"}
-              />
-            </Form.Group>
-            <Row>
-              
-              <Col>
-                
-                <Form.Group controlId="meetingStartDate">
-                  
-                  <Form.Label>Start Date</Form.Label>
-                  <Form.Control
-                    type="date"
-                    name="startDate"
-                    value={meetingData.startDate}
-                    onChange={handleInputChange}
+          <Formik
+            initialValues={meetingData}
+            validationSchema={validationSchema}
+            onSubmit={handleSave}
+            enableReinitialize
+          >
+            {({ handleSubmit, isSubmitting }) => (
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="meetingTitle">
+                  <Form.Label>Meeting Title</Form.Label>
+                  <Field
+                    name="title"
+                    as={Form.Control}
+                    type="text"
+                    placeholder="Enter meeting title"
                     readOnly={modalMode === "view"}
+                    isInvalid={!!ErrorMessage}
+                  />
+                  <ErrorMessage
+                    name="title"
+                    component="div"
+                    className="text-danger"
                   />
                 </Form.Group>
-              </Col>
-              <Col>
-                
-                <Form.Group controlId="meetingStartTime">
-                  
-                  <Form.Label>Start Time</Form.Label>
-                  <Form.Control
-                    type="time"
-                    name="startTime"
-                    value={meetingData.startTime}
-                    onChange={handleInputChange}
+                <Form.Group controlId="requiredEmails">
+                  <Form.Label>Required</Form.Label>
+                  <Field
+                    name="requiredEmails"
+                    as={Form.Control}
+                    type="text"
+                    placeholder="Enter required emails"
                     readOnly={modalMode === "view"}
                   />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              
-              <Col>
-                
-                <Form.Group controlId="meetingEndDate">
-                  
-                  <Form.Label>End Date</Form.Label>
-                  <Form.Control
-                    type="date"
-                    name="endDate"
-                    value={meetingData.endDate}
-                    onChange={handleInputChange}
-                    readOnly={modalMode === "view"}
+                  <ErrorMessage
+                    name="requiredEmails"
+                    component="div"
+                    className="text-danger"
                   />
                 </Form.Group>
-              </Col>
-              <Col>
-                
-                <Form.Group controlId="meetingEndTime">
-                  
-                  <Form.Label>End Time</Form.Label>
-                  <Form.Control
-                    type="time"
-                    name="endTime"
-                    value={meetingData.endTime}
-                    onChange={handleInputChange}
+                <Form.Group controlId="optionalEmails">
+                  <Form.Label>Optional</Form.Label>
+                  <Field
+                    name="optionalEmails"
+                    as={Form.Control}
+                    type="text"
+                    placeholder="Enter optional emails"
                     readOnly={modalMode === "view"}
                   />
+                  <ErrorMessage
+                    name="optionalEmails"
+                    component="div"
+                    className="text-danger"
+                  />
                 </Form.Group>
-              </Col>
-            </Row>
-            <Form.Group controlId="meetingLocation">
-              
-              <Form.Label>Location</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter location"
-                name="location"
-                value={meetingData.location}
-                onChange={handleInputChange}
-                readOnly={modalMode === "view"}
-              />
-            </Form.Group>
-          </Form>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="meetingStartDate">
+                      <Form.Label>Start Date</Form.Label>
+                      <Field
+                        name="startDate"
+                        as={Form.Control}
+                        type="date"
+                        readOnly={modalMode === "view"}
+                      />
+                      <ErrorMessage
+                        name="startDate"
+                        component="div"
+                        className="text-danger"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="meetingStartTime">
+                      <Form.Label>Start Time</Form.Label>
+                      <Field
+                        name="startTime"
+                        as={Form.Control}
+                        type="time"
+                        readOnly={modalMode === "view"}
+                      />
+                      <ErrorMessage
+                        name="startTime"
+                        component="div"
+                        className="text-danger"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="meetingEndDate">
+                      <Form.Label>End Date</Form.Label>
+                      <Field
+                        name="endDate"
+                        as={Form.Control}
+                        type="date"
+                        readOnly={modalMode === "view"}
+                      />
+                      <ErrorMessage
+                        name="endDate"
+                        component="div"
+                        className="text-danger"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="meetingEndTime">
+                      <Form.Label>End Time</Form.Label>
+                      <Field
+                        name="endTime"
+                        as={Form.Control}
+                        type="time"
+                        readOnly={modalMode === "view"}
+                      />
+                      <ErrorMessage
+                        name="endTime"
+                        component="div"
+                        className="text-danger"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Form.Group controlId="meetingLocation">
+                  <Form.Label>Location</Form.Label>
+                  <Field
+                    name="location"
+                    as={Form.Control}
+                    type="text"
+                    placeholder="Enter location"
+                    readOnly={modalMode === "view"}
+                  />
+                  <ErrorMessage
+                    name="location"
+                    component="div"
+                    className="text-danger"
+                  />
+                </Form.Group>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  {modalMode === "create" && (
+                    <Button
+                      variant="primary"
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      Save Meeting
+                    </Button>
+                  )}
+                </Modal.Footer>
+              </Form>
+            )}
+          </Formik>
         </Modal.Body>
-        <Modal.Footer>
-          
-          <Button variant="secondary" onClick={handleClose}>
-            
-            Close
-          </Button>
-          {modalMode === "create" && (
-            <Button variant="primary" onClick={handleSave}>
-              Save Meeting
-            </Button>
-          )}
-        </Modal.Footer>
       </Modal>
     </div>
   );
-}
-
-export default Calendar;
+};
+export default FullCalendarComponent;
