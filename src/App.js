9c909165aac0bde60272c34e42a9a61063984bf2
Elonmusk <srujanaj313@gmail.com>
2024-06-25 06:59:27 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Calendar from "./components/Calendar";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+      </BrowserRouter>
       <Calendar />
     </div>
   );
